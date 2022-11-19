dbfda87508c602ae47121d5ac2b4bdde0dafbef7
ahnaf <ahnaf@mail.local>
2022-11-19 22:29:26 +0700

    update api

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://192.168.18.23:7777';
+// export const API_URL = 'http://192.168.18.23:7777';
 // export const API_URL = 'https://geovox.herokuapp.com';
+export const API_URL = 'https://api.geovox.xyz';
